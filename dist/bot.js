1a0c7eecbc2cdab4dc9c22f08fa9326ea6bbe91f
Rafael Bueno <78382141+Runderground@users.noreply.github.com>
2025-02-17 23:58:58 +0000

    fix: some errors and added presence

--- dist/bot.js
+++ dist/bot.js
@@ -6,6 +6,10 @@ const error_1 = require("./embeds/error");
 const client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds, discord_js_1.GatewayIntentBits.GuildMessages] });
 client.once('ready', () => {
     console.log(`Bot está online como ${client.user?.tag}`);
+    client.user?.setActivity({
+        name: "Use /assinar para adquirir os Pontos de Decisão",
+        type: discord_js_1.ActivityType.Playing
+    });
 });
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand())
@@ -14,11 +18,17 @@ client.on('interactionCreate', async (interaction) => {
         const discordId = interaction.user.id;
         const username = interaction.user.username;
         const guild = await client.guilds.fetch(process.env.GUILD_ID);
-        if (!guild)
-            return interaction.reply({ embeds: [error_1.ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano')], ephemeral: true });
+        if (!guild) {
+            console.error("Erro ao buscar o servidor");
+            interaction.reply({ embeds: [error_1.ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano')], ephemeral: true });
+            return;
+        }
         const member = await guild.members.fetch(discordId);
-        if (!member)
-            return interaction.reply({ embeds: [error_1.ErrorEmbed.setTitle('❌ Ocorreu umn erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano')] });
+        if (!member) {
+            console.error("Erro ao buscar o membro");
+            interaction.reply({ embeds: [error_1.ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano')], ephemeral: true });
+            return;
+        }
         if (member.roles.cache.has(process.env.ROLE_ID))
             return interaction.reply({ embeds: [error_1.ErrorEmbed.setTitle('❌ Você já tem assinatura!').setDescription('Identificamos que você já tem acesso à assinatura, não é necessario assinar novamente.').setFooter({ text: "Qualquer problema, contate o suporte!" })], ephemeral: true });
         try {
@@ -66,10 +76,10 @@ client.on('interactionCreate', async (interaction) => {
             });
         }
         catch (error) {
-            await interaction.reply({
-                embeds: [error_1.ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Não consegui gerar um link de pagamento, tente novamente mais tarde...')],
+            /* await interaction.reply({
+                embeds: [ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Não consegui gerar um link de pagamento, tente novamente mais tarde...')],
                 ephemeral: true
-            });
+            }) */
             console.error(error);
         }
     }
--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
+import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, ActivityType, IntentsBitField } from 'discord.js';
 
 import { LinkEmbed } from './embeds/link';
 import { ErrorEmbed } from './embeds/error';
@@ -8,6 +8,11 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 client.once('ready', () => {
   console.log(`Bot está online como ${client.user?.tag}`);
+
+  client.user?.setActivity({
+    name: "Use /assinar para adquirir os Pontos de Decisão",
+    type: ActivityType.Playing
+  })
 })
 
 client.on('interactionCreate', async (interaction) => {
@@ -19,11 +24,19 @@ client.on('interactionCreate', async (interaction) => {
 
     const guild = await client.guilds.fetch(process.env.GUILD_ID as string);
     
-    if(!guild) return interaction.reply({ embeds: [ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano' )], ephemeral: true })
+    if(!guild) {
+      console.error("Erro ao buscar o servidor");
+      interaction.reply({ embeds: [ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano' )], ephemeral: true })
+      return 
+    }
     
     const member = await guild.members.fetch(discordId);
 
-    if(!member) return interaction.reply({ embeds: [ErrorEmbed.setTitle('❌ Ocorreu umn erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano' )] })
+    if(!member) {
+      console.error("Erro ao buscar o membro");
+      interaction.reply({ embeds: [ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Ocorreu um erro ao tentar assinar o plano' )], ephemeral: true })
+      return 
+    }
 
     if(member.roles.cache.has(process.env.ROLE_ID as string)) return interaction.reply({ embeds: [ErrorEmbed.setTitle('❌ Você já tem assinatura!').setDescription('Identificamos que você já tem acesso à assinatura, não é necessario assinar novamente.' ).setFooter({text: "Qualquer problema, contate o suporte!"})], ephemeral: true})
 
@@ -80,10 +93,10 @@ client.on('interactionCreate', async (interaction) => {
         ephemeral: true
       })
     } catch (error) {
-      await interaction.reply({
+      /* await interaction.reply({
           embeds: [ErrorEmbed.setTitle('❌ Ocorreu um erro inesperado').setDescription('Não consegui gerar um link de pagamento, tente novamente mais tarde...')],
           ephemeral: true
-      })
+      }) */
       console.error(error)
     }
   }
